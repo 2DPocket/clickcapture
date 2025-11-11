e0cfb4cc9778d3aeff4eb8cfe29fb57107dea7d8
aoemon.sama2 <aoemon.sama2@gmail.com>
2025-11-11 17:56:13 +0900

    リリースモードコンパイルした場合は、コマンドプロンプトウィンドウを表示しないようにした

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 /*
 ============================================================================
 ClickCapture - Windows Screen Capture Tool with Area Selection (main.rs)
